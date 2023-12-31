1c3e620f4630ec5e24bb8fc75847e42871b10acb
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2023-12-31 17:58:28 +0700

    Register and beginning of login

--- Python/accounts/urls.py
+++ Python/accounts/urls.py
@@ -5,7 +5,8 @@ from accounts import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup/', views.register, name="signup")
+    path('signup/', views.register, name="signup"),
+    path('login/', views.login, name="login")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Python/accounts/views.py
+++ Python/accounts/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from Tomato.models import Listing
 from Tomato.models import Booking
 from django.http import HttpResponse
 
 def register(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                  form.save()
+                  #log the user in
+                  return redirect("/listings/")
+        else:
+            form = UserCreationForm()      
+        return render(request, 'register.html', {'form': form})
 
-        return render(request, 'register.html')
+def login(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(data = request.POST)
+        else:
+            form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
