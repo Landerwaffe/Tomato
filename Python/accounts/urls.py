80968cda2eb8b67901db041b9f37a7e2f64a86b4
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-01-01 13:31:54 +0700

    Logout

--- Python/accounts/urls.py
+++ Python/accounts/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('signup/', views.register, name="signup"),
-    path('login/', views.login, name="login")
+    path('login/', views.signin, name="login"),
+    path('logout/', views.signout, name="logout")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Python/accounts/views.py
+++ Python/accounts/views.py
@@ -3,21 +3,34 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from Tomato.models import Listing
 from Tomato.models import Booking
 from django.http import HttpResponse
+from django.contrib.auth import login, logout
 
 def register(request):
         if request.method == 'POST':
             form = UserCreationForm(request.POST)
             if form.is_valid():
-                  form.save()
+                  user = form.save()
                   #log the user in
+                  login(request, user)
                   return redirect("/listings/")
         else:
             form = UserCreationForm()      
         return render(request, 'register.html', {'form': form})
 
-def login(request):
+def signin(request):
         if request.method == 'POST':
             form = AuthenticationForm(data = request.POST)
+            if form.is_valid():
+                # log in the user
+                user = form.get_user()
+                login(request, user)
+                return redirect("/listings/")
         else:
             form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
+
+def signout(request):
+     if request.method == 'POST':
+        logout(request)
+        return redirect("/listings/")
+         
