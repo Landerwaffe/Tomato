84b72765f59c6052d93b546a6ef9479425115029
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-01-07 21:57:59 +0700

    Changed home to root directory

--- Python/Tomato/urls.py
+++ Python/Tomato/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),  
+    path('', views.home),  
     path('listings/', views.listings),
     path('contact/', views.contact),
     path('about/', views.about),
